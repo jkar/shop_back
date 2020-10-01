9c60902f7c6f2aa6dd9ccb1c365204754e717d0b
jkar <gianniskaramitsos3@gmail.com>
2020-10-01 19:37:25 +0300

    set get count

--- Routers/productRouter.js
+++ Routers/productRouter.js
@@ -1,13 +1,31 @@
 const productsRouter = require('express').Router();
 const Product = require('../Models/products');
 
+productsRouter.get('/count', async (req, res, next) => {
+    try {
+        const limit = req.body.limit;
+        const count = await Product.count();
+        let numberOfPages = Math.floor( count / limit );
+        if (count % limit !== 0) {
+            numberOfPages = numberOfPages + 1;
+        }
+
+        res.status(200).send( { numberOfPages : numberOfPages });
+    } catch (err) {
+        res.status(400).send({ msg : err });
+    }
+});
+
 productsRouter.get('/', async (req, res, next) => {
 
+    try {
     const offset = req.body.offset;
     const limit = req.body.limit
-    console.log('req', req.body);
     const result = await Product.find().skip(offset).limit(limit);
     res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send({msg : err});
+    }
 });
 
 productsRouter.post('/', async (req, res, next) => {
