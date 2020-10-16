54512ae83351b2158eeed54f84ffae6cf4440b8b
Karamitsos <ikaramitsos@deloitte.gr>
2020-10-16 13:38:38 +0300

    userRouter without params

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -7,18 +7,24 @@ productsRouter.post('/', async (req, res, next) => {
     try {
     console.log('req', req.body);
     const saltRounds = await bcrypt.genSaltSync(10);
-    const passwordHash = await bcrypt.hashSync(req.body.params.password, saltRounds);
+    // const passwordHash = await bcrypt.hashSync(req.body.params.password, saltRounds);
+    const passwordHash = await bcrypt.hashSync(req.body.password, saltRounds);
 
+    // const u = {
+    //     name : req.body.params.name,
+    //     username : req.body.params.username,
+    //     password : passwordHash
+    // }
     const u = {
-        name : req.body.params.name,
-        username : req.body.params.username,
+        name : req.body.name,
+        username : req.body.username,
         password : passwordHash
     }
     const user = new User(u);
     const result = await user.save();
     res.status(200).json(result);
     } catch (err) {
-        res.status(400).send({ msg : err.message})
+        return res.status(400).json({ msg : err.message});
     }
 });
 
