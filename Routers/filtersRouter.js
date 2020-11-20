4c8f15897964f040c1847d12c12b6b886eb94906
jkar <gianniskaramitsos3@gmail.com>
2020-11-21 01:27:48 +0200

    filter for title

--- Routers/filtersRouter.js
+++ Routers/filtersRouter.js
@@ -13,4 +13,22 @@ filtersRouter.get('/', async (req, res, next) => {
 
 });
 
+filtersRouter.get('/title', async (req, res, next) => {
+
+    try {
+         const listOfTitles = req.query.titles;
+        // console.log('TITLES', listOfTitles);
+        console.log('BODY', req.body);
+        console.log('QUERY', req.query);
+
+        const result = await Product.find({
+            // "title": { $in: ["shocks", "boots"] }
+            "title": { $in: listOfTitles }
+        });
+        res.status(200).send(result);
+    } catch (error) {
+        res.status(400).send({ msg : error });
+    }
+});
+
 module.exports = filtersRouter;
