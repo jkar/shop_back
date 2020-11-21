d8d8a85fe926cfdb746c5e54d05c9df96f747508
jkar <gianniskaramitsos3@gmail.com>
2020-11-21 23:03:07 +0200

    offset limit on filtered titles and count for buttons

--- Routers/filtersRouter.js
+++ Routers/filtersRouter.js
@@ -17,17 +17,36 @@ filtersRouter.get('/title', async (req, res, next) => {
 
     try {
          const listOfTitles = req.query.titles;
-        // console.log('TITLES', listOfTitles);
-        console.log('BODY', req.body);
-        console.log('QUERY', req.query);
+         const offset = parseInt(req.query.offset);
+         const limit = parseInt(req.query.limit);
 
         const result = await Product.find({
             // "title": { $in: ["shocks", "boots"] }
             "title": { $in: listOfTitles }
-        });
+        }).sort({date: -1}).skip(offset).limit(limit);
         res.status(200).send(result);
     } catch (error) {
         res.status(400).send({ msg : error });
+    }
+});
+
+filtersRouter.get('/countTitles', async (req, res, next) => {
+    try {
+        const listOfTitles = req.query.titles;
+        const limit = req.query.limit;
+        console.log('listOfTitles', listOfTitles);
+        console.log('limit', limit);
+        const count = await Product.find({
+            "title": { $in: listOfTitles }
+        }).countDocuments();
+        let numberOfPages = Math.floor( count / limit );
+        if (count % limit !== 0) {
+            numberOfPages = numberOfPages + 1;
+        }
+
+        res.status(200).send( { numberOfPages : numberOfPages });
+    } catch (err) {
+        res.status(400).send({ msg : err });
     }
 });
 
